bcc6fca4d1ac964e1101e81883e298aa99c82f00
panda <2066476165@qq.com>
2019-07-16 19:44:55 +0800

    修改

--- src/per/demo/gittest.java
+++ src/per/demo/gittest.java
@@ -3,6 +3,6 @@ package per.demo;
 public class gittest {
 	public static void main(String[] args) {
 		System.out.println("hello,github");
-		System.out.println("第一次修改");
+		System.out.println("一次修改");
 	}
 }
